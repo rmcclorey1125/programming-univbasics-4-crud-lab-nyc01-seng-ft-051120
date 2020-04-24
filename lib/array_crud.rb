11f0ff1cdb7c1682d745491dc6fe8406142e2240
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-24 13:17:49 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays!")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number] = "totally"
+  array[index_number] = element
 end
